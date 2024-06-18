88a2678af11a001eb6fd1d8db2f73c9db088e5d3
Manoele Ribeiro <137442585+ManoeleRibeiro@users.noreply.github.com>
2024-06-18 12:16:39 -0300

    18-06

--- main.js
+++ main.js
@@ -5,5 +5,19 @@ const caixaResultado = document.querySelector ('.caixa-resultado');
 const textoResultado = document.querySelector ('.texto-resultado');
 
 const perguntas = [
-    
+    {
+        enuciado:"Assim que saiu da escola,você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?",
+        alternativas:[
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    }
+    {
+        enunciado:"pergunta1",
+        alternativas: [
+            "alternativa1",
+            "alternativa2"
+        ]
+    }
+    "pergunta2"
 ]
